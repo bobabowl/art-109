03cb12a97a2f46180dcb422f54dff9aeee0ef022
bobabowl <angelotadeo32@gmail.com>
2025-03-02 17:46:22 -0800

    added project 2

--- projects/project-2-tadeo/js/main.js
+++ projects/project-2-tadeo/js/main.js
@@ -1,4 +1,9 @@
 function handleDropdownChange(level) {
+    const currentDropdownId = `dropdown${level}-container`;
+    if (document.getElementById(currentDropdownId)) {
+        document.getElementById(currentDropdownId).classList.remove("visible");
+        document.getElementById(currentDropdownId).classList.add("used");
+    }
     const nextDropdownId = `dropdown${level + 1}-container`;
     // Show the next dropdown if it exists
     if (document.getElementById(nextDropdownId)) {
@@ -6,4 +11,16 @@ function handleDropdownChange(level) {
         document.getElementById(nextDropdownId).classList.remove("hidden");
         document.getElementById(nextDropdownId).classList.add("visible");
     }
+}
+
+function finalPrompt() {
+    const currentDropdownId = `dropdown15-container`;
+    if (document.getElementById(currentDropdownId)) {
+        document.getElementById(currentDropdownId).classList.remove("visible");
+        document.getElementById(currentDropdownId).classList.add("used");
+    }
+    // document.querySelector(".background").style.backgroundColor = "white"
+    document.getElementById(`bg`).classList.remove("background");
+    document.getElementById(`bg`).classList.add("background-alt");
+
 }
